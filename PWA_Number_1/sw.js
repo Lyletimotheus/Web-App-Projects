01bf55d75875a0962244adfc42038f81f379e2c4
Lyle Timotheus <lyle6494@gmail.com>
2021-08-06 14:02:45 +0200

    Small changes

--- PWA_Number_1/sw.js
+++ PWA_Number_1/sw.js
@@ -7,3 +7,8 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
     console.log('Service worker has been activated.');
 });
+
+// Fetch events - getting files from the server such as JS,HTML,CSS
+self.addEventListener('fetch', evt => {
+    console.log('fetch event');
+}); 
